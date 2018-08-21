eae0ac45449f85bf56890025170acf6e6cfa4689
Saguni <BSaguni@ad.harman.com>
2018-08-21 16:25:37 +0530

    all the file deleted'

--- p15.py
+++
@@ -1,8 +0,0 @@
-string='hello world good morning'
-result=string.split(' ')
-for word in result:
-    length =len(word)
-    if (length %2!=0):
-         print('word : {} and length is : {}'.format(word,length))
-         print('it is even length!!!!')
-   
